0153d74f1fdcb4d0da5b2c2f1d039fe4b352ad0f
Dartanhan Ramos <alessandroramosd@gmail.com>
2024-11-23 15:41:35 -0500

    Refactored breedFetcher and added tests for fetchBreedDescription

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,22 @@
 const needle = require('needle');
 
-const args = process.argv.slice(2);
-const breedName = args[0];
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`; // The API endpoint for searching breeds
 
-
-// The API endpoint for searching breeds
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-
-// Fetch the breed data
-needle.get(url, (error, response) => {
-  if (error) {
-    console.log('Error:', error);
-  } else {
-    const body = response.body;
-
-    if (body.length === 0) {
-      console.log(`No breeds were found.`);
+  // Fetch the breed data
+  needle.get(url, (error, response) => {
+    if (error) {
+      callback(error, null); // Pass error to the callback
     } else {
-      console.log(body[0].description); // Print the description of the first breed
+      const body = response.body;
+
+      if (body.length === 0) {
+        callback(`No breeds were found`, null); // Pass error message
+      } else {
+        callback(null, body[0].description); // Pass description
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
